65b13fc5c6968a143c18c4af2790df5cdb35c444
BayuXD-Tunnel <bayuriadi82@smp.belajar.id>
2024-12-10 14:37:23 +0700

    Update qris.js

--- qris.js
+++ qris.js
@@ -31,7 +31,7 @@ function generateTransactionId() {
 
 function generateExpirationTime() {
     const expirationTime = new Date();
-    expirationTime.setMinutes(expirationTime.getMinutes() + 30);
+    expirationTime.setMinutes(expirationTime.getMinutes() + 5);
     return expirationTime;
 }
 
